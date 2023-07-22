8b8b697b3ad98004181b813ef79947d1d29a58b0
samithaf <fernando.samitha@gmail.com>
2023-07-22 10:40:20 +1000

    first commit

---
+++ keystatic.config.ts
@@ -0,0 +1,16 @@
+import { config, fields, singleton } from '@keystatic/core'
+
+export default config({
+    storage: {
+        kind: 'local',
+    },
+    singletons: {
+        homepage: singleton({
+            label: 'Homepage',
+            path: 'src/content/_homepage',
+            schema: {
+                headline: fields.text({ label: 'Headline' }),
+            },
+        }),
+    },
+})
