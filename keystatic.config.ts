88b3754185a0d474aecde1c5e299976c2830db8f
Samitha Fernando <samitha.fernando@westpac.com.au>
2023-10-25 20:15:03 +1100

    updated the base to align with official example

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,16 +1,35 @@
-import { config, fields, singleton } from '@keystatic/core'
+import {config, collection, fields, LocalConfig, GitHubConfig} from '@keystatic/core';
+
+// Storage strategy
+const storage: LocalConfig['storage'] | GitHubConfig['storage'] =
+    process.env.NODE_ENV === 'development'
+        ? { kind: 'local' }
+        : {
+          kind: 'github',
+          repo: {
+            owner: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER!,
+            name: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG!,
+          },
+        }
 
 export default config({
-    storage: {
-        kind: 'local',
-    },
-    singletons: {
-        homepage: singleton({
-            label: 'Homepage',
-            path: 'src/content/_homepage',
-            schema: {
-                headline: fields.text({ label: 'Headline' }),
-            },
+  storage,
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.document({
+          label: 'Content',
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: true,
         }),
-    },
-})
+      },
+    }),
+  },
+});
