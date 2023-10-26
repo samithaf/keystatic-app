9ff255a4cb7719c31a08ed43a193c17f4f5e5c0d
Samitha Fernando <samitha.fernando@westpac.com.au>
2023-10-26 16:00:33 +1100

    print all

--- apps/site/app/design-system/[...component]/page.tsx
+++ apps/site/app/design-system/[...component]/page.tsx
@@ -1,4 +1,5 @@
 import {reader} from "../../reader";
+import Link from "next/link";
 
 
 type Props = {
@@ -7,17 +8,17 @@ type Props = {
 export default async function Page({params}: Props) {
     const {component} = params;
     console.log('component', component);
-    const collection = await reader.collections.designSystem.all();
-    const matchingComponent = collection.find(post => post.slug === component.join('/'));
-
-    if(!matchingComponent) {
-        return <p>Sorry!</p>
-    }
-
+    const designs = await reader.collections.designSystem.all();
     return (
         <>
             <p>Looking up article {component.join('/')}</p>
-            <p>params {JSON.stringify(matchingComponent)}</p>
+            <ul>
+                {designs.map(post => (
+                    <li key={post.slug}>
+                        {post.entry.name}
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
