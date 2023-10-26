7cd05050010958eae01a220403f28f88bd22dbd2
Samitha Fernando <samitha.fernando@westpac.com.au>
2023-10-26 17:11:37 +1100

    log process.cwd()

--- apps/site/src/app/keystatic/reader.ts
+++ apps/site/src/app/keystatic/reader.ts
@@ -1,4 +1,6 @@
 import { createReader } from '@keystatic/core/reader';
 import keystaticConfig from './keystatic.config';
 
+
+console.log('process.cwd()', process.cwd());
 export const reader = createReader(process.cwd(), keystaticConfig);
