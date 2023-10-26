bcf10595492f16d0624893a9cbd6066cadb4bbda
Samitha Fernando <samitha.fernando@westpac.com.au>
2023-10-26 16:22:41 +1100

    fixes the compile issue

--- apps/site/src/app/api/keystatic/[...params]/route.ts
+++ apps/site/src/app/api/keystatic/[...params]/route.ts
@@ -1,5 +1,6 @@
 import { makeRouteHandler } from '@keystatic/next/route-handler';
-import keystaticConfig from '../../../../keystatic.config';
+import keystaticConfig from "../../../keystatic/keystatic.config";
+
 
 export const { POST, GET } = makeRouteHandler({
   config: keystaticConfig,
